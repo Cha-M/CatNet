f76dcc893dad132f0a70db4a20220d35910d51af
Sha <charlotte.megroff@outlook.com>
2022-02-17 12:31:29 +0000

    footer component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import Flex from './Flex';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
 
