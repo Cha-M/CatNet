30dd44504581c7cc08cef7d34e2e943be711d0f4
joelc95 <joelconalty@gmail.com>
2022-02-17 14:31:13 +0000

    removed webvitals from index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 import Flex from './Flex';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 
