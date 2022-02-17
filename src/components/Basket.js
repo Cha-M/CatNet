be0e35d8e8c1e8d64f94bf56af554bc9a6f9124d
Ethan Frediani <91057765+EethanF@users.noreply.github.com>
2022-02-17 14:22:09 +0000

    Update Basket.js

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,6 +1,6 @@
 import React from "react"
 // hooks and funcs need to go in root component
-// add buttons to cat items
+// add funcs to cat item buttons
 // const [basketItems, setBasketItems] = useState([]);
 
 
@@ -19,8 +19,8 @@ const Basket = (props) =>{
             <h2>Your Basket</h2>
             <div className = "item">{basketItems.length === 0 && <p>Basket Is Empty</p>}</div>
             {basketItems.map((item) =>(
-                <div key = {cat.id}>
-                <p>{cat.name}</p>
+                <div key = {item.id}>
+                <p>{item.name}</p>
                 <img src = {item.image} alt = {item.name}/>
                 <button onClick ={() => addItem(item)}>+</button>
                 <button onClick ={() => removeItem(item)}>-</button>
