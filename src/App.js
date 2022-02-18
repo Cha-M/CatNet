5a7d31a994c9788e78ed5c00e309e7087d2e2627
joelc95 <joelconalty@gmail.com>
2022-02-18 12:18:46 +0000

    github and shopping cart svgs added

--- src/App.js
+++ src/App.js
@@ -96,7 +96,10 @@ function App() {
       {/* RENDER HEADER NAV BAR */}
       <div className="header-container">
         <Header />
-        <button onClick={basketModalHandler}>Basket</button>
+        {/* <button onClick={basketModalHandler}>Basket</button> */}
+        <a class="cart" onClick={basketModalHandler}><svg className="svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg></a>
       </div>
 
       <div className='basket-container'>
@@ -104,8 +107,11 @@ function App() {
         {/* RENDER MODAL BASKET WHEN CLICKED */}
         <Modal style={custom} isOpen={basketModal}>
           <div className='basket-contents'>
-            <button id='close-basket' onClick={basketModalHandler}>x</button>
-            <h2>Your Basket</h2>
+            <div className='basket-header'>
+              <button id='close-basket' onClick={basketModalHandler}>x</button>
+              <h2 className='basket-title'>Your Basket</h2>
+            </div>
+            
             <div className = "basket-items">
               
               {basket.map( (item, index) => {
@@ -137,8 +143,8 @@ function App() {
             <div className="cat" key={index}>
               <img src={kitty.img}></img>
               <div className="cat-info">
-                <p> {kitty.name} | £{kitty.price} </p>
-                <p onClick={() => addToBasket(kitty)}>[+]</p>
+                <p className="name-and-price"> {kitty.name} | £{kitty.price} </p>
+                <p className="add-cat-button" onClick={() => addToBasket(kitty)}>[+]</p>
               </div>
             </div>
           )
